7a76aa16cdab4fc8e433eb6246da9638bff8685c
odon-airoldi <odon.airoldi@gmail.com>
2026-02-05 15:21:07 +0100

    initialization forlder and file

---
+++ dadi/assets/js/main.js
@@ -0,0 +1,9 @@
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
